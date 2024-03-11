fe71a25ae4abf49108b9edea5d948efc37345f3d
Mikudayo <146194087+yatagaratsu666@users.noreply.github.com>
2024-03-11 10:15:32 -0500

    Fixed

--- src/main/java/brenda/priorityqueue/PriorityQueue.java
+++ src/main/java/brenda/priorityqueue/PriorityQueue.java
@@ -10,10 +10,6 @@ public class PriorityQueue<E> extends AbstractPriorityQueue<E> {
     private Array<Queue<E>> priority;
     private int prioridades;
     private int size = 0;
-<<<<<<< HEAD
-=======
-    private int inode = 0;
->>>>>>> ec87ac3c5318b00459cad26bbeb1c97516ce0656
 
     public PriorityQueue(int imtData) {
         this.size = 0;
@@ -75,64 +71,29 @@ public class PriorityQueue<E> extends AbstractPriorityQueue<E> {
 
     @Override
     public boolean insert(E element) {
-<<<<<<< HEAD
         priority.get(prioridades - 1).insert(element);
         size++;
         return true;
-=======
-        if (this.priority.size() > inode) {
-            size++;
-            inode++;
-            return this.priority.get(0).insert(element);
-        } else {
-            return false;
-        }
->>>>>>> ec87ac3c5318b00459cad26bbeb1c97516ce0656
     }
 
     @Override
     public boolean insert(int index, E element) {
-<<<<<<< HEAD
         priority.get(index).insert(element);
         size++;
         return true;
-=======
-        if (index >= 0 && index < this.priority.size() && this.size < this.prioridades) {
-            size++;
-            Queue<E> targetQueue = this.priority.get(index);
-            targetQueue.insert(element);
-            return true;
-        } else {
-            return false;
-        }
->>>>>>> ec87ac3c5318b00459cad26bbeb1c97516ce0656
     }
 
     @Override
     public E peek() {
-<<<<<<< HEAD
         for (int i = 0; i < prioridades; i++) {
             if (priority.get(i).size() != 0) {
                 return priority.get(i).peek();
-=======
-        return this.priority.get(0).peek();
-    }
-
-    @Override
-    public E extract() {
-        for (int i = 0; i < this.priority.size(); i++) {
-            Queue<E> current = this.priority.get(i);
-            if (!current.isEmpty()) {
-                size--;
-                return current.extract();
->>>>>>> ec87ac3c5318b00459cad26bbeb1c97516ce0656
             }
         }
         return null;
     }
 
     @Override
-<<<<<<< HEAD
     public E extract() {
         for (int i = 0; i < prioridades; i++) {
             if (priority.get(i).size() != 0) {
@@ -147,9 +108,4 @@ public class PriorityQueue<E> extends AbstractPriorityQueue<E> {
     public int size() {
         return size;
     }
-=======
-    public int size() {
-        return size;
-    }
->>>>>>> ec87ac3c5318b00459cad26bbeb1c97516ce0656
 }
