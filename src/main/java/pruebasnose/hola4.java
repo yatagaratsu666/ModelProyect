ec87ac3c5318b00459cad26bbeb1c97516ce0656
Mikudayo <146194087+yatagaratsu666@users.noreply.github.com>
2024-03-11 01:19:30 -0500

    added new tests

--- src/main/java/pruebasnose/hola4.java
+++ src/main/java/pruebasnose/hola4.java
@@ -1,12 +1,32 @@
-
 package pruebasnose;
 
-import java.util.PriorityQueue;
-
-
+import brenda.priorityqueue.PriorityQueue;
+import brenda.util.iterator.Iterator;
 
 public class hola4 {
     public static void main(String[] args) {
-        PriorityQueue<String> priorityQueue = new PriorityQueue<>();
+        PriorityQueue<String> priorityQueue = new PriorityQueue<>(3);
+        System.out.println(priorityQueue.size());
+        priorityQueue.insert(0, "Miku");
+        priorityQueue.insert(2, "Rin");
+        priorityQueue.insert(1, "Len");
+        
+        System.out.println(priorityQueue.size());
+        Iterator<String> iterator = priorityQueue.iterator();
+        while (iterator.hasNext()) {
+            String task = iterator.next();
+            System.out.println(task);
+        }
+        priorityQueue.extract();
+        priorityQueue.extract();
+        priorityQueue.extract();
+//        System.out.println(priorityQueue.extract());
+//        System.out.println(priorityQueue.extract());
+//        System.out.println(priorityQueue.extract());
+        System.out.println(priorityQueue.size());
+        while (iterator.hasNext()) {
+            String task = iterator.next();
+            System.out.println(task);
+        }
     }
 }
